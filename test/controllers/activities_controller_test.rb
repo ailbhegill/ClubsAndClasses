d592504f0e1f646505ac5a080b1dced712e634bf
ailbhegill <ailbhe.g@hotmail.com>
2016-10-11 23:58:40 +0100

    fixed broken tests

--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -18,7 +18,7 @@ class ActivitiesControllerTest < ActionController::TestCase
 
   test "should create activity" do
     assert_difference('Activity.count') do
-      post :create, activity: { address: @activity.address, contact_no: @activity.contact_no, contact_person: @activity.contact_person, county: @activity.county, days_and_times: @activity.days_and_times, description: @activity.description, email: @activity.email, image_url: @activity.image_url, name: @activity.name, price_per_class: @activity.price_per_class }
+      post :create, activity: { address: @activity.address, contact_no: @activity.contact_no, contact_person: @activity.contact_person, county: @activity.county, days_and_times: @activity.days_and_times, description: @activity.description, email: @activity.email, image_url: @activity.image_url, name: "New Activity", price_per_class: @activity.price_per_class }
     end
 
     assert_redirected_to activity_path(assigns(:activity))
