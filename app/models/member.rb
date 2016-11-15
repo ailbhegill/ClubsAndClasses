4165ee75552228cfa194681310e1775bbe6ed084
ailbhegill <ailbhe.g@hotmail.com>
2016-11-15 14:48:50 +0000

    ensured only single enrollments possible

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,12 @@
 class Member < ActiveRecord::Base
-	has_many :enrollments, dependent: :destroy
+  has_many :enrollments, dependent: :destroy
+  def add_activity(activity_id)
+    current_enroll = enrollments.find_by(activity_id: activity_id)
+    if current_enroll
+      #do nothing for now
+    else
+      current_enroll = enrollments.build(activity_id: activity_id)
+    end
+    current_enroll
+  end
 end
